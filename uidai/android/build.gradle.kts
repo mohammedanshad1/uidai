f95a24a2f3414686f511990114630d49efb0e9d8
mohammedanshads <anshad@xynosoft.com>
2026-08-06 14:54:07 +0530

    feat: initialize root build.gradle.kts with Android and Kotlin project configuration

--- uidai/android/build.gradle.kts
+++ uidai/android/build.gradle.kts
@@ -3,31 +3,38 @@ buildscript {
         google()
         mavenCentral()
     }
-
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     }
 }
-
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
 }
-
 rootProject.layout.buildDirectory.set(File("../build"))
-
 subprojects {
     val newSubprojectBuildDir = rootProject.layout.buildDirectory.dir(project.name)
     project.layout.buildDirectory.set(newSubprojectBuildDir)
 }
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
-
+subprojects {
+    afterEvaluate {
+        tasks.withType<JavaCompile>().configureEach {
+            sourceCompatibility = "17"
+            targetCompatibility = "17"
+        }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
+}
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
