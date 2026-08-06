fdb8fbdf11ffbbf93c5aa1c32b3e631faa66113c
mohammedanshads <anshad@xynosoft.com>
2026-08-06 15:31:24 +0530

    feat: add Android build configuration and Gradle project setup

--- uidai/android/build.gradle.kts
+++ uidai/android/build.gradle.kts
@@ -23,15 +23,13 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 subprojects {
-    afterEvaluate {
-        tasks.withType<JavaCompile>().configureEach {
-            sourceCompatibility = "17"
-            targetCompatibility = "17"
-        }
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
         }
     }
 }
